e6db9296a50e0575f1512c77a9935f2b6ee779f0
noshababooher <noshababooher7@gmail.com>
2024-05-16 18:46:40 +0500

    add class1.dart & if else.dart

---
+++ if else.dart
@@ -0,0 +1,14 @@
+void main() {
+  int age = 23;
+  int number = 22;
+
+  if (age > number) {
+    print('number is less than age');
+  } else if (number == age) {
+    print('number is equal to age');
+  } else if (number != age) {
+    print('number is not equal age ');
+  } else {
+    print('number is grater than age');
+  }
+}
