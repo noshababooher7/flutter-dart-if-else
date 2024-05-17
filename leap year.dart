0dab30b74284f0207930988c2a8b8ec8da5952f7
noshababooher <noshababooher7@gmail.com>
2024-05-17 15:15:56 +0500

    add leap year

---
+++ leap year.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  int year = 2024;
+  if (year % 4 == 0) {
+    print('this is leap year $year');
+  }else if (year % 5==4){
+
+  }else {
+    print('this is not leap year $year');
+  }
+}
