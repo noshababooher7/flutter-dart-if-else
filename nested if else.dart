c3b9fb1c57b07e3e018e2d1d43e6e43f165bac81
noshababooher <noshababooher7@gmail.com>
2024-05-19 08:34:50 +0500

    update nested if else

---
+++ nested if else.dart
@@ -0,0 +1,21 @@
+
+
+void main() {
+  String email = "Noshababooher7@gmail.com";
+  String pasword = "1234567";
+
+
+  if (email == "Noshababooher7@gmail.com" && pasword == "1234567") {
+
+    if (email == "Noshababooher7@gmail.com" && pasword == "1234567") {
+      print("login successed in both ");
+    } else if (email == "Noshababooher7@gmail.com") {
+      print("login in email");
+    } else {
+      print("login in pasword");
+    }
+
+  } else {
+    print("login is faild");
+  }
+}
